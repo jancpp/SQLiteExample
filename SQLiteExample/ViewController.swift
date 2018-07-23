4d19d4c4120e2d3d32628d65727e5e5e6b920a0d
Jan Polzer <janman74@gmail.com>
2018-07-22 23:56:43 -0500

    Created DB with table

--- SQLiteExample/ViewController.swift
+++ SQLiteExample/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController {
+    
+    var db: OpaquePointer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileUrl = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("HeroDatabase.sqlite")
+        
+        if sqlite3_open(fileUrl.path, &db) != SQLITE_OK {
+            print("Error opening database")
+        }
+        
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS Heros (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, powerrank INTEGER)"
+        
+        if sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK {
+            print("Error creating table")
+            return
+        }
+        
+        print("Everything worked")
     }
 
     override func didReceiveMemoryWarning() {
