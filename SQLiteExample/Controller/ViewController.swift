64f8dd1d7fff851dc2b4d2c4c360c5e8efae34e6
Jan Polzer <janman74@gmail.com>
2018-07-25 09:08:25 -0500

    TableView displaying data from local SQLite

--- SQLiteExample/Controller/ViewController.swift
+++ SQLiteExample/Controller/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 import SQLite3
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
     
     var db: OpaquePointer?
+    var heroList = [Hero]()
     
+    @IBOutlet weak var tableViewHeroes: UITableView!
     @IBOutlet weak var textFieldName: UITextField!
     @IBOutlet weak var textFieldRanking: UITextField!
+    
     @IBAction func buttonSave(_ sender: UIButton) {
         let name = textFieldName.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         let powerrank = textFieldRanking.text?.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -46,13 +49,20 @@ class ViewController: UIViewController {
         }
         
         if sqlite3_step(stmt) == SQLITE_DONE {
-            print("Hero saved succesfully")
+            print("Hero saved succesfully in \(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))")
         }
+        
+        readValues()
+        textFieldName.text = ""
+        textFieldRanking.text = ""
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableViewHeroes.delegate = self
+        tableViewHeroes.dataSource = self
         
         let fileUrl = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
             .appendingPathComponent("HeroDatabase.sqlite")
@@ -69,13 +79,51 @@ class ViewController: UIViewController {
         }
         
         print("Everything worked")
+        readValues()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func readValues() {
+        
+        // Empty the list of heroes
+        heroList.removeAll()
+        
+        // Select query
+        let queryString = "SELECT * FROM Heroes"
+        
+        // Statement pointer
+        var stmt:OpaquePointer?
+        
+        // Prepare the query
+        if sqlite3_prepare(db, queryString, -1, &stmt, nil) != SQLITE_OK {
+            let errmsg = String(cString: sqlite3_errmsg(db)!)
+            print("error preparing insert: \(errmsg)")
+            return
+        }
+        
+        // Traverse through all the records
+        while(sqlite3_step(stmt) == SQLITE_ROW) {
+            let id = sqlite3_column_int(stmt, 0)
+            let name = String(cString: sqlite3_column_text(stmt, 1))
+            let powerrank = sqlite3_column_int(stmt, 2)
+            
+            // Add values to list
+            heroList.append(Hero(id: Int(id), name: String(describing: name), powerranking: Int(powerrank)))
+        }
+        self.tableViewHeroes.reloadData()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return heroList.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        let hero: Hero
+        hero = heroList[indexPath.row]
+        cell.textLabel?.text = hero.name
+        
+        return cell
+    }
 }
 
